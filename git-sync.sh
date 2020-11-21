3ba11af42f91811812fcd8ddc91b0b13c21cd7ec
lytex <julianlpc15@gmail.com>
2020-11-21 11:54:17 +0100

    notification on sync conflicts

--- git-sync.sh
+++ git-sync.sh
@@ -15,15 +15,19 @@ is_command() {
 
 if [ is_command termux_info ]; then
     AM="am" # termux activity manager
+    NOTIF_CMD="termux-notification"
+    NOTIF_CONFLICT="$NOTIF_CMD -c 'sync conflict!' --id 'sync-conflict'"
 else
     AM="true" # Disable command
-
+    # TODO Configure with notify-send
+    NOTIF_CMD="true"
+    NOTIF_CONFLICT="true"
 
 INW="inotifywait";
 EVENTS="close_write,move,delete,create";
 INCOMMAND="\"$INW\" -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\""
 
-for cmd in "git" "$INW" "timeout" "$AM"; do
+for cmd in "git" "$INW" "timeout" "$AM" "$NOTIF_CMD"; do
     is_command "$cmd" || { stderr "Error: Required command '$cmd' not found"; exit 1; }
 done
 
@@ -33,7 +37,7 @@ echo "$INCOMMAND"
 
 while true; do
     eval "timeout 600 $INCOMMAND" || true
-    git pull
+    git pull || $NOTIF_CONFLICT
     sleep 5
     STATUS=$(git status -s)
     if [ -n "$STATUS" ]; then
@@ -41,7 +45,7 @@ while true; do
         echo "commit!"
         git add .
         git commit -m "autocommit `git config user.name`@`date +'%Y-%m-%d %H:%M:%S'`"
-        git push origin
+        git push origin || $NOTIF_CONFLICT
         $AM startservice -a android.intent.action.MAIN -n com.orgzly/com.orgzly.android.sync.SyncService
     fi
 done
