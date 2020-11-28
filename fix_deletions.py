8a3f9547102a6c27d28ba93c5825d93a4fc491d1
lytex <julianlpc15@gmail.com>
2020-11-28 18:01:58 +0100

    change to PREVIOUS_DIR

--- fix_deletions.py
+++ fix_deletions.py
@@ -9,14 +9,13 @@ from git import Repo
 load_dotenv()
 
 ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
-print(123)
+PREVIOUS_DIR = os.getcwd()
 os.chdir(ORG_DIRECTORY)
-print(456)
 
 repo = Repo(ORG_DIRECTORY)
-print(444)
 
 last_commit = next(repo.iter_commits())
+os.chdir(PREVIOUS_DIR)
 
 if last_commit.stats.total["deletions"] - last_commit.stats.total["insertions"] > 50:
     sys.exit(1)
