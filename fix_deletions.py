ccc684739aa41417c0d205a7dcea5bd3619b1eca
lytex <julianlpc15@gmail.com>
2020-12-06 16:51:44 +0100

    detect uncommitted deleted files instead

--- fix_deletions.py
+++ fix_deletions.py
@@ -7,6 +7,7 @@ from git import Repo
 load_dotenv()
 
 ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
+ORG_DIRECTORY = "."
 PREVIOUS_DIR = os.getcwd()
 os.chdir(ORG_DIRECTORY)
 
@@ -15,7 +16,7 @@ repo = Repo(ORG_DIRECTORY)
 last_commit = next(repo.iter_commits())
 os.chdir(PREVIOUS_DIR)
 
-if (deleted := list(filter(lambda x: x.a_blob is None, repo.index.diff(repo.head.commit.parents[0])))) != []:
+if (deleted := list(filter(lambda x: x.deleted_file, repo.index.diff(None)))) != []:
     for file in deleted:
         print(f'"{file.a_path}"', end=" ")
     sys.exit(1)
