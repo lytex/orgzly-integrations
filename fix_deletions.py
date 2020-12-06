00c2fcd277346e81634a9d169184ffad78157268
lytex <julianlpc15@gmail.com>
2020-12-06 16:19:26 +0100

    python script to detect committed deleted files

--- fix_deletions.py
+++ fix_deletions.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python3
-
 import os
 import sys
 
@@ -9,6 +7,7 @@ from git import Repo
 load_dotenv()
 
 ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
+ORG_DIRECTORY = "."
 PREVIOUS_DIR = os.getcwd()
 os.chdir(ORG_DIRECTORY)
 
@@ -17,7 +16,9 @@ repo = Repo(ORG_DIRECTORY)
 last_commit = next(repo.iter_commits())
 os.chdir(PREVIOUS_DIR)
 
-if last_commit.stats.total["deletions"] - last_commit.stats.total["insertions"] > 50:
+if (deleted := list(filter(lambda x: x.a_blob is None, repo.index.diff(repo.head.commit.parents[0])))) != []:
+    for file in deleted:
+        print(f'"{file.a_path}"', end=" ")
     sys.exit(1)
 else:
     sys.exit(0)
