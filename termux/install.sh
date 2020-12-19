6ec19c2a619f3f3d1fd88634dfc28bfaa13248ea
SM-A50 <you@example.com>
2020-12-19 21:44:59 +0100

    add tail_log.sh script

--- termux/install.sh
+++ termux/install.sh
@@ -4,6 +4,7 @@ cp kill_wrapper.sh ~/.shortcuts
 cp launch_git-sync.sh ~/.shortcuts
 cp launch_git-sync.sh ~/.shortcuts/tasks
 cp remove_notifs.sh ~/.shortcuts/tasks
+cp tail_log.sh ~/.shortcuts
 
 
 termux-fix-shebang ../git-sync.sh
