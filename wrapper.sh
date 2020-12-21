d204ac0af843afeb8eadea31c2d7c4cea44fbb7d
lytex <julianlpc15@gmail.com>
2020-12-21 13:40:46 +0100

    remove unused NOTIF_RESTART

--- wrapper.sh
+++ wrapper.sh
@@ -6,7 +6,6 @@ if command -v "termux-info" &>/dev/null ; then
     NOTIF_CMD="termux-notification"
     NOTIF_ERROR="$NOTIF_CMD -t git-sync -c ERROR --id error --ongoing"
     NOTIF_START="$NOTIF_CMD -t git-sync_started"
-    NOTIF_RESTART="$NOTIF_CMD -t git-sync_restarted"
 elif [ "$(uname -m)" == "armv7l" ]; then
     NOTIF_CMD="echo `date`"
     NOTIF_ERROR="$NOTIF_CMD git-sync ERROR -t 0"
