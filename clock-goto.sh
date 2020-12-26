e93c291ac4222194f4d1b30d0b30f3429c2ba642
lytex <julianlpc15@gmail.com>
2020-12-27 00:01:50 +0100

    remove quotes

--- clock-goto.sh
+++ clock-goto.sh
@@ -25,7 +25,7 @@ update_clock_goto_notification() {
 
 }
 
-INCOMMAND="inotifywait -qr -e close_write,move,delete,create --exclude \"\.git\" $ORG_DIRECTORY"
+INCOMMAND="inotifywait -qr -e close_write,move,delete,create --exclude \.git $ORG_DIRECTORY"
 cd $ORG_DIRECTORY
 
 while true; do
