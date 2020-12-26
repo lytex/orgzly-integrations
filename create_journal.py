a9e6ce74b9283c8eef66084ee8ea30fd6e8cf714
lytex <julianlpc15@gmail.com>
2020-12-27 00:58:19 +0100

    use local time with pytz

--- create_journal.py
+++ create_journal.py
@@ -4,12 +4,13 @@ import os
 import locale
 from datetime import datetime
 from dotenv import load_dotenv
+from pytz import timezone
 
 load_dotenv()
 
 ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
 
-dt = datetime.now()
+dt = datetime.now(tz=timezone("Europe/Madrid"))
 locale.setlocale(locale.LC_ALL, "es_ES.utf8")
 with open(dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org"), "w") as journal:
     print(dt.strftime("%A, %d de %B de %Y"), file=journal)
