72f05bbc468fcd8f3c03b7be5a2769a2f2f3588a
lytex <julianlpc15@gmail.com>
2020-12-27 00:51:14 +0100

    fix wrong argument order

--- create_journal.py
+++ create_journal.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-import locale
 import os
+import locale
 from datetime import datetime
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -12,5 +11,5 @@ ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
 
 dt = datetime.now()
 locale.setlocale(locale.LC_ALL, "es_ES.utf8")
-with open(dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org", "w")) as journal:
+with open(dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org"), "w") as journal:
     print(dt.strftime("%A, %d de %B de %Y"), file=journal)
