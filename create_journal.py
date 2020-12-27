9205028f0bbbb34af90593a8b5e662ac3e2f1413
lytex <julianlpc15@gmail.com>
2020-12-27 01:00:36 +0100

    add *

--- create_journal.py
+++ create_journal.py
@@ -13,4 +13,4 @@ ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
 dt = datetime.now(tz=timezone("Europe/Madrid"))
 locale.setlocale(locale.LC_ALL, "es_ES.utf8")
 with open(dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org"), "w") as journal:
-    print(dt.strftime("%A, %d de %B de %Y"), file=journal)
+    print(dt.strftime("* %A, %d de %B de %Y"), file=journal)
