703e109f58517b9389210f4b6479ce9660170995
lytex <julianlpc15@gmail.com>
2021-05-24 22:38:10 +0200

    add script to sync termux now

---
+++ termux/sync_now.sh
@@ -0,0 +1,7 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+cd /data/data/com.termux/files/home/orgzly-integrations/
+# Launch git-sync if it's now already running
+grep wrapper.sh || ./wrapper.sh
+source .env
+touch "$ORGZLY_FILE_INDEX"
