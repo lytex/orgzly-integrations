e95322a4174d48efc3f50ef39e7f0f0e5b8fc42b
lytex <julianlpc15@gmail.com>
2021-05-24 23:34:31 +0200

    fix wrong command in sync_now, install to ~/.shortucts/tasks

--- termux/install.sh
+++ termux/install.sh
@@ -6,7 +6,7 @@ cp launch_clock-goto.sh ~/.shortcuts
 # cp launch_git-sync.sh ~/.shortcuts/tasks # Disabled, not working
 cp remove_notifs.sh ~/.shortcuts/tasks
 cp tail_log.sh ~/.shortcuts
-cp sync_now.sh ~/.shortcuts
+cp sync_now.sh ~/.shortcuts/tasks
 
 
 termux-fix-shebang ../git-sync.sh
