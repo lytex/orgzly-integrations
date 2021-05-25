086d3edeb24f8e3644603d650d2c446a650ea729
lytex <julianlpc15@gmail.com>
2021-05-25 10:52:59 +0200

    wait for inotify

--- termux/sync_now.sh
+++ termux/sync_now.sh
@@ -3,5 +3,6 @@
 cd /data/data/com.termux/files/home/orgzly-integrations/
 # Launch git-sync if it's now already running
 (ps -e | grep wrapper.sh) || (setsid ./wrapper.sh &)
+while ! (ps -e | grep "inotifywait"); do sleep 1; done # Wait for inotifywait
 source .env
 touch "$ORGZLY_FILE_INDEX"
