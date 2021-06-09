28ff6d4fbb67d55d87f01c0a6da3851db355147c
lytex <julianlpc15@gmail.com>
2021-06-09 23:58:24 +0200

    fix order in recursive build_index

--- LectureNotesIndex.py
+++ LectureNotesIndex.py
@@ -9,7 +9,7 @@ def ls(path: str):
     files_and_dirs = os.listdir(path)
     files_and_dirs = [os.path.join(path, x) for x in files_and_dirs]
     dirs, files = list(filter(isdir, files_and_dirs)), list(filter(lambda x: isfile(x), files_and_dirs))
-    dirs = [x.replace(os.path.join(path + ""), "") for x in dirs]
+    dirs = [x.replace(os.path.join(path, ""), "") for x in dirs]
     files = [x.replace(os.path.join(path, ""), "") for x in files]
     yield path, dirs, files
 
@@ -21,19 +21,19 @@ def lowercase(s: str):
 def build_index(path: str, level: int) -> Iterable[str]:
     print(path)
     path = list(ls(path))
-    print(path)
+    # print(path)
 
     for root, directories, files in path:
         directories, files = sorted(directories, key=lowercase), sorted(files, key=lowercase)
 
         for directory in directories:
             yield "*" * (level + 1) + f" {directory}"
-            yield from sorted(build_index(root + "/" + directory, level + 1), key=lowercase)
+            yield from build_index(os.path.join(root, directory), level + 1)
 
         for file in files:
             if file.endswith(".png") and file.startswith("page"):
-                link = root.replace(LECTURE_NOTES_DIRECTORY, "")[1:] + "/" + file
-                yield "*" * (level + 1) + f" {file}\n[[file:{link}][{file}]]"
+                link = os.path.join(root.replace(LECTURE_NOTES_DIRECTORY, ""), file)
+                yield "*" * (level + 1) + f" {file}\n[[file:{link}]]"
 
 
 load_dotenv()
