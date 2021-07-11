f1f2524d4cb918806ff638086af95e999290adb7
lytex <julianlpc15@gmail.com>
2021-07-11 22:15:58 +0200

    add script to export org mode files

--- export.sh
+++ export.sh
@@ -4,7 +4,7 @@ source .env
 cd "$ORG_DIRECTORY"
 EVENTS="close_write,move,delete,create";
 INCOMMAND="inotifywait -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\""
-while true;
+while true; do
     files=$(eval $INCOMMAND | grep -i *.org)
     for file in "$files"; do
         echo emacs --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
