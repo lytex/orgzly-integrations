7129051774caf6c42e60ea2e91c4606ca48b727f
lytex <julianlpc15@gmail.com>
2021-07-11 23:07:54 +0200

    eval and load

--- export.sh
+++ export.sh
@@ -5,7 +5,7 @@ cd "$ORG_DIRECTORY"
 EVENTS="close_write,move,delete,create";
 INCOMMAND="inotifywait -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\" --format \"%w%f\""
 while true; do
-    files=$(eval $INCOMMAND | grep -i *.org)
+    files=$(eval $INCOMMAND | grep -i "\.org$")
     echo $files
     for file in "$files"; do
         emacs --batch --eval="(progn (load-file \"$HOME/.emacs.d/early-init.el\") (load-file \"$HOME/.emacs.d/init.el\" ) (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
