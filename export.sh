eafd56ae3a6db4972e924d0195bd2b2188f419a2
lytex <julianlpc15@gmail.com>
2021-07-11 22:24:03 +0200

    export in batch mode

--- export.sh
+++ export.sh
@@ -7,7 +7,7 @@ INCOMMAND="inotifywait -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\"
 while true; do
     files=$(eval $INCOMMAND | grep -i *.org)
     for file in "$files"; do
-        echo emacs --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
-        emacs --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
+        echo emacs --batch --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
+        emacs --batch --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
     done
 done
