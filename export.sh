50e6df77b9de7dd1bd80a293183762986fff4568
lytex <julianlpc15@gmail.com>
2021-07-11 22:40:09 +0200

    inw format, execute config script

--- export.sh
+++ export.sh
@@ -3,11 +3,12 @@
 source .env
 cd "$ORG_DIRECTORY"
 EVENTS="close_write,move,delete,create";
-INCOMMAND="inotifywait -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\""
+INCOMMAND="inotifywait -qr -e \"$EVENTS\" --exclude \"\.git\" \"$ORG_DIRECTORY\" --format \"%w%f\""
 while true; do
     files=$(eval $INCOMMAND | grep -i *.org)
+    echo $files
     for file in "$files"; do
-        echo emacs --batch --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
-        emacs --batch --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
+        echo emacs --script "$HOME/.doom.d/config.el" --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
+        emacs --script "$HOME/.doom.d/config.el" --eval="(progn (find-file \"$file\") (org-transclusion-mode t) (org-html-export-to-html))"
     done
 done
