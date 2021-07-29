c388a785582149840df1a5b340c12823cd4d6823
lytex <julianlpc15@gmail.com>
2021-07-29 21:59:48 +0200

    retry commit only if COMMIT_RESULT=1

--- git-sync.sh
+++ git-sync.sh
@@ -92,8 +92,12 @@ git_add_commit() {
         # Also add || true (see above)
         user_date="$(git config user.name)@$(date +'%Y-%m-%d %H:%M:%S')"
         changed_files=$(git status -s | awk '{$1=""; print $0}' | tr -d '\n')
-        git commit -m "autocommit $user_date $changed_files" || true
-        echo $((try++)) > /dev/null
+        COMMIT_RESULT=$(git commit -m "autocommit $user_date $changed_files") || true
+        if (( COMMIT_RESULT != 0 )); then
+            echo $((try++)) > /dev/null
+        else
+            break
+        fi
     done
 }
 
