47fa8554ebc9350b68f8bcf236e3c5bb28e1193f
lytex <julianlpc15@gmail.com>
2021-07-29 18:29:36 +0200

    use also git push errors as part of PUSH_RESULT

--- git-sync.sh
+++ git-sync.sh
@@ -103,7 +103,7 @@ git_push () {
 
     echo "git push" >> "$LOGFILE"
     PUSH_CODE=0 # Establish default value, only replace if its different from 0
-    PUSH_RESULT=$(git push) || PUSH_CODE=${PUSH_CODE:-$(check_conflict "$?")}
+    PUSH_RESULT=$(git push 2>&1) || PUSH_CODE=${PUSH_CODE:-$(check_conflict "$?")}
     echo $PUSH_RESULT >> "$LOGFILE"
 }
 
