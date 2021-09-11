ab4863ccbbc3e41050fde0a0069222d2c57f977b
lytex <julianlpc15@gmail.com>
2021-09-11 08:42:32 +0200

    do not use a function

--- caldav.sh
+++ caldav.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-function retry_sync() {
+false # This makes $? != 0 at the beggining of the while loop
 
-    false # This makes $? != 0 at the beggining of the while loop
+while (($? != 0)); do
 
-    while (($? != 0)); do
-        emacs -l "$HOME/.emacs.d/early-init.el" -l "$HOME/.emacs.d/init.el" --batch \
-            --eval="(progn (add-to-list 'auth-sources \"~/.auth info\") (org-caldav-sync-calendar) (org-save-all-org-buffers))"
-    done
+    timeout 1h \
+    emacs -l "$HOME/.emacs.d/early-init.el" -l "$HOME/.emacs.d/init.el" --batch \
+    --eval="(progn (add-to-list 'auth-sources \"~/.auth info\") (org-caldav-sync-calendar) (org-save-all-org-buffers))"
 
-}
-
-timeout 1h retry_sync
-
+done
