19e16b18750a8da7052e0f3f1e9da1a45d5a3175
lytex <julianlpc15@gmail.com>
2021-10-12 23:03:43 +0200

    create journal only if not exists

--- create_journal.py
+++ create_journal.py
@@ -12,5 +12,7 @@ ORG_DIRECTORY = os.getenv("ORG_DIRECTORY")
 
 dt = datetime.now(tz=timezone("Europe/Madrid"))
 locale.setlocale(locale.LC_ALL, "es_ES.utf8")
-with open(dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org"), "w") as journal:
-    print(dt.strftime("* %A, %d de %B de %Y"), file=journal)
+journal_file = dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org")
+if not os.path.isfile(journal_file):
+    with open(journal_file, "w") as journal:
+        print(dt.strftime("* %A, %d de %B de %Y"), file=journal)
