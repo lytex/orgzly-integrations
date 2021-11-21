01f5612613ea145472ec4b93b24596c34eb018ed
lytex <julianlpc15@gmail.com>
2021-11-21 18:17:45 +0100

    add title

--- create_journal.py
+++ create_journal.py
@@ -15,4 +15,4 @@ locale.setlocale(locale.LC_ALL, "es_ES.utf8")
 journal_file = dt.strftime(f"{ORG_DIRECTORY}/journal/%Y-%m-%d.org")
 if not os.path.isfile(journal_file):
     with open(journal_file, "w") as journal:
-        print(dt.strftime("* %A, %d de %B de %Y"), file=journal)
+        print(dt.strftime("#+TITLE: %Y-%m-%d\n* %A, %d de %B de %Y"), file=journal)
