f300709022d8e1dbad7527f56afbc721e33a0e92
lytex <julianlpc15@gmail.com>
2023-09-17 11:44:20 +0200

    remove org-trello-sync.sh

--- org-trello-sync.sh
+++
@@ -1,4 +0,0 @@
-files=$(cat .org-trello-files)
-for file in $files; do 
-    emacs --batch --eval="(progn (load-file \"$HOME/.emacs.d/early-init.el\") (load-file \"$HOME/.emacs.d/init.el\" ) (find-file \"$file\") (org-trello-mode t) (org-trello-sync-buffer t))"
-done
