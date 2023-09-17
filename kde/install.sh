5aeaa5454353185c56c3872ed5ace7315a2ab7c3
lytex <julianlpc15@gmail.com>
2023-09-17 11:35:56 +0200

    remove kde/install.sh

--- kde/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat wrapper.sh | sed 's|\#\!/bin/bash|#!/bin/bash\n'"GIT_SYNC_DIRECTORY=$(pwd)|" > ${XDG_CONFIG_HOME:-$HOME/.config}/autostart-scripts/git-sync.sh
-chmod +x ${XDG_CONFIG_HOME:-$HOME/.config}/autostart-scripts/git-sync.sh
-
-echo "#!/bin/bash\nkillall wrapper.sh\nkillall git-sync.sh" > ${XDG_CONFIG_HOME:-$HOME/.config}/plasma-workspace/shutdown/exit-git-sync.sh
-chmod +x ${XDG_CONFIG_HOME:-$HOME/.config}/plasma-workspace/shutdown/exit-git-sync.sh
